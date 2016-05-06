ed73fcba39f9a42bbf695dde80b3bb03c25e75ae
Eduardo Straub <eduardo.straub1@gmail.com>
2016-05-05 22:57:34 -0300

    Se le agrego lo de la Hoz.

---
+++ app.js
@@ -0,0 +1,28 @@
+var express  = require("express"),
+    app      = express(),
+    http     = require("http"),
+    server   = http.createServer(app),
+    mongoose = require('mongoose'),
+    tropasModels = require("./node_modules/models/tropasModels");
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+   res.send("Hello World!");
+});
+
+router.get('/perro', function(req, res) {
+   res.send("Hello Perro!");
+});
+
+
+app.use(router);
+
+mongoose.connect('mongodb://localhost/tropas', function(err, res) {
+  if(err) {
+    console.log('ERROR: connecting to Database. ' + err);
+  }
+  app.listen(3000, function() {
+    console.log("Node server running on http://localhost:3000");
+  });
+});
