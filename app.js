9926d682605740a652accfb2063e1e9c45abac46
Eduardo Straub <eduardo.straub1@gmail.com>
2016-05-06 03:34:57 -0300

    arreglo

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ var express  = require("express"),
     http     = require("http"),
     server   = http.createServer(app),
     mongoose = require('mongoose'),
-    tropasModels = require("./node_modules/models/tropasModelo");
+    tropasModels = require("./node_modules/models/tropasModelo")
+    bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 var router = express.Router();
 
@@ -15,6 +20,20 @@ router.get('/perro', function(req, res) {
    res.send("Hello Perro!");
 });
 
+router.post('/evento', function(req, res, next){
+	var nuevoEvento = new Evento({
+		nombreUsurio: req.decoded._doc.nombreUsuario,
+	    categoria: req.body.categoria,
+	    descripcion: req.body.descripcion
+
+	});
+
+	nuevoEvento.save(function(err, evento) {
+		if(err) return res.send(500, err.message);
+
+                res.json(evento);
+	});
+});
 
 app.use(router);
 
